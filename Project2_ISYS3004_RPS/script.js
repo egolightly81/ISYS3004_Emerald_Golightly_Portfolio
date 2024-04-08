e724d471f0ffecd59b4ea1be8533b47c855321c0
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-08 15:29:13 +0800

    js in the project file producing an 1 M message

--- Project2_ISYS3004_RPS/script.js
+++ Project2_ISYS3004_RPS/script.js
@@ -1,25 +1,25 @@
 // Determine the result based on the user's choice
-    let result = "";
-    if (user === 'rock') {
-        result = "tie";
-        draws++;
-    } else if (user === 'paper') {
-        result = "win";
-        wins++;
-    } else if (user === 'scissors') {
-        result = "loses"; // Consider changing this to "loses" for correct spelling
-        loses++;
-    }
+let result = "";
+if (user === 'rock') {
+    result = "tie";
+    draws++;
+} else if (user === 'paper') {
+    result = "win";
+    wins++;
+} else if (user === 'scissors') {
+    result = "loses"; // Consider changing this to "loses" for correct spelling
+    loses++;
+}
 
-    // Construct the message with game outcome and scores
-    let mymessage = "Computer chose rock, you " + result + "!" + 
-        "\n\n" + wins + " wins" +
-        "\n\n" + loses + " loses" +
-        "\n\n" + draws + " draws";
-    
-    // Display the message in an alert and speak it
-    alert(mymessage);
-    say(mymessage);
+// Construct the message with game outcome and scores
+let mymessage = "Computer chose rock, you " + result + "!" + 
+    "\n\n" + wins + " wins" +
+    "\n\n" + loses + " loses" +
+    "\n\n" + draws + " draws";
+
+// Display the message in an alert and speak it
+alert(mymessage);
+say(mymessage);
 }
 
 // Variables to keep track of wins, loses, and draws
@@ -29,6 +29,6 @@ let draws = 0;
 
 // Function to speak a message using SpeechSynthesis API
 function say(themessage) {
-    let msg = new SpeechSynthesisUtterance(themessage);
-    speechSynthesis.speak(msg);
+let msg = new SpeechSynthesisUtterance(themessage);
+speechSynthesis.speak(msg);
 }
